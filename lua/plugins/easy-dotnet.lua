0f015012891796e83e0c9f2b11940ea77801662c
Stefan <5796872+stefandango@users.noreply.github.com>
2025-03-20 21:14:24 +0100

    Updated dotnet outdated run

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,7 +1,36 @@
 return {
-  "GustavEikaas/easy-dotnet.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  dir = "~/Dev/nvimplugins/easy-dotnet.nvim", -- Required
+  name = "easy-dotnet", -- Optional
   config = function()
     require("easy-dotnet").setup()
+    vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+      pattern = { "Directory.Packages.props", "*.csproj" },
+      callback = function()
+        require("easy-dotnet").outdated()
+      end,
+    })
   end,
 }
+-- return {
+--   "GustavEikaas/easy-dotnet.nvim",
+--   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+--   config = function()
+--     local function get_secret_path(secret_guid)
+--       local path = ""
+--       local home_dir = vim.fn.expand("~")
+--       if require("easy-dotnet.extensions").isWindows() then
+--         local secret_path = home_dir
+--           .. "\\AppData\\Roaming\\Microsoft\\UserSecrets\\"
+--           .. secret_guid
+--           .. "\\secrets.json"
+--         path = secret_path
+--       else
+--         local secret_path = home_dir .. "/.microsoft/usersecrets/" .. secret_guid .. "/secrets.json"
+--         path = secret_path
+--       end
+--       return path
+--     end
+--     local dotnet = require("easy-dotnet")
+--     dotnet.setup({})
+--   end,
+-- }
