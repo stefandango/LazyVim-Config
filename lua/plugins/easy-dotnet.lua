b6fc24744530ff783f3af1362f9d566cbe69676f
Stefan <5796872+stefandango@users.noreply.github.com>
2025-04-13 21:00:18 +0200

    added close shortcut to window

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -5,24 +5,36 @@ return {
     local dotnet = require("easy-dotnet")
     dotnet.setup(
       {
-        mappings = {
-          run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
-          filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
-          debug_test = { lhs = "<leader>d", desc = "debug test" },
-          go_to_file = { lhs = "g", desc = "got to file" },
-          run_all = { lhs = "<leader>R", desc = "run all tests" },
-          run = { lhs = "<leader>r", desc = "run test" },
-          peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
-          expand = { lhs = "o", desc = "expand" },
-          expand_node = { lhs = "E", desc = "expand node" },
-          expand_all = { lhs = "-", desc = "expand all" },
-          collapse_all = { lhs = "W", desc = "collapse all" },
-          close = { lhs = "q", desc = "close testrunner" },
-          refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" },
-        },
-        --- Optional table of extra args e.g "--blame crash"
-        additional_args = {},
+        ---@param action "test" | "restore" | "build" | "run" | "watch"
+        terminal = function(path, action, args)
+          local commands = {
+            run = function()
+              return string.format("dotnet run --project %s %s", path, args)
+            end,
+            test = function()
+              return string.format("dotnet test %s %s", path, args)
+            end,
+            restore = function()
+              return string.format("dotnet restore %s %s", path, args)
+            end,
+            build = function()
+              return string.format("dotnet build %s %s", path, args)
+            end,
+            watch = function()
+              return string.format("dotnet watch --project %s %s", path, args)
+            end,
+          }
+
+          local command = commands[action]() .. "\r"
+          vim.cmd("split")
+          vim.cmd("term " .. command)
+          vim.cmd("startinsert")
+
+          local current_buf = vim.api.nvim_get_current_buf()
+          vim.api.nvim_buf_set_keymap(current_buf, "n", "<C-q>", ":close<CR>", { noremap = true, silent = true })
+        end,
       },
+
       -- Example keybinding
       vim.keymap.set("n", "<C-p>", function()
         dotnet.run()
